3dec397f9123879b0a5680ed8eea849bbc70cc54
Smolanov-Sergey <smolanovsergey@gmail.com>
2020-11-24 11:26:22 +0200

    gulp,style + scripts added

--- wp-content/themes/life-tree/functions.php
+++ wp-content/themes/life-tree/functions.php
@@ -142,8 +142,10 @@ add_action( 'widgets_init', 'life_tree_widgets_init' );
 function life_tree_scripts() {
 	wp_enqueue_style( 'life-tree-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'life-tree-style', 'rtl', 'replace' );
+    wp_enqueue_style( 'life-tree-css', get_template_directory_uri() . '/dist/css/style.css', array(), _S_VERSION );
+    wp_enqueue_script('life-tree-script', get_template_directory_uri() . '/dist/js/common.js', array(), _S_VERSION );
 
-	wp_enqueue_script( 'life-tree-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+//	wp_enqueue_script( 'life-tree-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
